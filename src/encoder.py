869316d0a866ba74bf25b3589a727b6803b290f6
pq <devdsinf@gmail.com>
2023-07-19 08:12:15 +0800

    update filepath for mapper.py and encorder.py for deployment

--- src/encoder.py
+++ src/encoder.py
@@ -4,7 +4,7 @@ sys.path.append("../")
 
 class CityEncoder:
     def __init__(self):
-        self.city_encoder = joblib.load('artifact/label_encoder_city.pkl')
+        self.city_encoder = joblib.load('../artifact/label_encoder_city.pkl')
 
     def transform(self, input_features):
         return self.city_encoder.transform(input_features)
@@ -12,7 +12,7 @@ class CityEncoder:
 
 class ZipcodeEncoder:
     def __init__(self):
-        self.zipcode_encoder = joblib.load('artifact/label_encoder_zipcode.pkl')
+        self.zipcode_encoder = joblib.load('../artifact/label_encoder_zipcode.pkl')
 
     def transform(self, input_features):
         return self.zipcode_encoder.transform(input_features)
--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ sys.path.append("../")
 
 class Model:
     def __init__(self):
-        self.model = joblib.load('artifact/catboost_model.pkl')
+        self.model = joblib.load('../artifact/catboost_model.pkl')
 
     def predict(self, input_features):
         return self.model.predict(input_features)
